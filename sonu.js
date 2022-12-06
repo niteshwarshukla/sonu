97604b1650bdbb4b5a1861227273b093572c1127
niteshwarshukla <119488577+niteshwarshukla@users.noreply.github.com>
2022-12-06 21:10:08 +0530

    Update sonu.js

---
+++ sonu.js
@@ -0,0 +1,17 @@
+let count =0;
+for
+    (let i=1; i<=0; i++);
+{
+    if (num[i]==0)
+ {
+     count ++;
+ }
+  if (count ==2);
+  {
+  
+   consol.log (yes);
+  }
+  
+  else {
+      console.log("No")
+      }
